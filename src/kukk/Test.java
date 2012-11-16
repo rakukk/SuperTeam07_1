ea6515fd62625f8bc4c525b0212ec5b75a4cd702
rakukk1 <rakukk@itcollege.ee>
2012-11-16 15:09:40 +0200

    proov

--- src/kukk/Test.java
+++ src/kukk/Test.java
@@ -20,7 +20,8 @@ public class Test extends HttpServlet {
 		
 		
 		//https://github.com/rakukk/SuperTeam07_1.git
-		
+		//https://github.com/rakukk/SuperTeam07_1.git
+		//https://github.com/rakukk/SuperTeam07_1.git
 		
 		
 	}
